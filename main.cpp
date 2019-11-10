9a968718e1ed5d110d8006582c3308473e98a9e1
nikkimoteva <37060980+nikkimoteva@users.noreply.github.com>
2019-11-09 17:14:18 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,15 +8,15 @@ using namespace std;
 
 int main()
 {
-    Person* shahram = new Person("shahram", 49, "M", vector<Person*>(), NULL, NULL);
-    Person* behazin = new Person ("behazin", 46, "F", vector<Person*>(), NULL, NULL);
+    Person* shahram = new Person("shahram", 49, "M", vector<Person*>(), vector<Person*>(), NULL, NULL);
+    Person* behazin = new Person ("behazin", 46, "F", vector<Person*>(), vector<Person*>(), NULL, NULL);
 	
     shahram->addSpouse(behazin);
     behazin->addSpouse(shahram);
 	
-    Person* nikki = new Person ("nikki", 20, "F", vector<Person*>(), shahram, behazin);
-    Person* lily = new Person ("Lily", 14, "F", vector<Person*>(), shahram, behazin);
-    Person* amin = new Person ("amin", 4, "M", vector<Person*>(), shahram, behazin);
+    Person* nikki = new Person ("nikki", 20, "F", vector<Person*>(), vector<Person*>(), shahram, behazin);
+    Person* lily = new Person ("Lily", 14, "F", vector<Person*>(), vector<Person*>(), shahram, behazin);
+    Person* amin = new Person ("amin", 4, "M", vector<Person*>(), vector<Person*>(), shahram, behazin);
 
     shahram->addChild(nikki);
     shahram->addChild(lily);
