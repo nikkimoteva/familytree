15e29c334dfe5a32d6fd50d6cf6930140f0cff75
nikkimoteva <37060980+nikkimoteva@users.noreply.github.com>
2019-11-16 23:02:29 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,30 +8,61 @@ using namespace std;
 
 int main()
 {
-    Person* shahram = new Person("shahram", 49, "M", vector<Person*>(), vector<Person*>(), NULL, NULL);
-    Person* behazin = new Person ("behazin", 46, "F", vector<Person*>(), vector<Person*>(), NULL, NULL);
-	
-    shahram->addSpouse(behazin);
-    behazin->addSpouse(shahram);
-	
-    Person* nikki = new Person ("nikki", 20, "F", vector<Person*>(), vector<Person*>(), shahram, behazin);
-    Person* lily = new Person ("Lily", 14, "F", vector<Person*>(), vector<Person*>(), shahram, behazin);
-    Person* amin = new Person ("amin", 4, "M", vector<Person*>(), vector<Person*>(), shahram, behazin);
+    Person* emily = new Person ("emily", 75, "F", vector<Person*>(), vector<Person*>(), NULL, NULL);
+    Person* richard = new Person ("richard", 78, "M", vector<Person*>(), vector<Person*>(), NULL, NULL);
+    emily->addSpouse(richard);
 
-    shahram->addChild(nikki);
-    shahram->addChild(lily);
-    shahram->addChild(amin);
+    Person* joey = new Person("joey", 49, "M", vector<Person*>(), vector<Person*>(), NULL, NULL);
+    Person* rachel = new Person ("rachel", 46, "F", vector<Person*>(), vector<Person*>(), richard, emily);
+    joey->addSpouse(rachel);
+    rachel->addSpouse(joey);
 
-    behazin->addChild(nikki);
-    behazin->addChild(lily);
-    behazin->addChild(amin);
+    Person* nikki = new Person ("nikki", 20, "F", vector<Person*>(), vector<Person*>(), joey, rachel);
+    Person* lily = new Person ("Lily", 14, "F", vector<Person*>(), vector<Person*>(), joey, rachel);
+
+    Person* ross = new Person ("ross", 45, "M", vector<Person*>(), vector<Person*>(), NULL, NULL);
+    Person* sara = new Person ("sara", 43, "F", vector<Person*>(), vector<Person*>(), NULL, NULL);
+    Person* jake = new Person ("jake", 6, "M", vector<Person*>(), vector<Person*>(), ross, sara);
+    Person* heather = new Person ("heather", 4, "F", vector<Person*>(), vector<Person*>(), ross, sara);
+
+    Person* luke = new Person ("luke", 52, "M", vector<Person*>(), vector<Person*>(), NULL, NULL);
+    Person* lorelai = new Person ("lorelai", 50, "F", vector<Person*>(), vector<Person*>(), richard, emily);
+    Person* collin = new Person ("collin", 22, "M", vector<Person*>(), vector<Person*>(), luke, lorelai);
+    Person* sam = new Person ("sam", 15, "M", vector<Person*>(), vector<Person*>(), luke, lorelai);
     
-    shahram->displayAll();
-    behazin->displayAll();
+    joey->addSibling(ross);
+    rachel->addSibling(lorelai);
+    
+    ross->addSpouse(sara);
+    luke->addSpouse(lorelai);
 
+    emily->addChild(lorelai);
+    emily->addChild(rachel);
+
+    joey->addChild(nikki);
+    joey->addChild(lily);
+
+    luke->addChild(collin);
+    luke->addChild(sam);
+
+    ross->addChild(jake);
+    ross->addChild(heather);
+
+    jake->addSibling(heather);
     nikki->addSibling(lily);
-    nikki->addSibling(amin);
+    collin->addSibling(sam);
+    
+    joey->displayAll();
+    lorelai->displayAll();
+    sara->displayAll();
+
     nikki->displaySiblings();
+    nikki->displayCousins();
+    cout << "\n";
+    nikki->displayAncestors();
+
+    cout << "\n";
+    emily->displayDescendants();
 	
     cout << "Press enter (or Ctrl-C) to quit ... " << endl;
     cin.get();
